c4d30b39528c29d3f038f9ef1b38d6630efef278
Elard Guevara <elard.guevara@ucsp.edu.pe>
2015-11-03 09:39:44 -0500

    avance

--- e5.cpp
+++ e5.cpp
@@ -6,7 +6,11 @@ int main(int argc, char const *argv[])
 {
 	
 	enum Days { Saturday,Sunday,Tuesday,Wednesday,Thursday,Friday }; 
-	enum Pos {Thursday, Friday, Sunday};//redeclarado
+	//enum Pos {Thursday, Friday, Sunday};//redeclarado
+
+	Days dia = Saturday;
+
+	dia = 453; //no permite
 
 	return 0;
 }
