3e86353702e1fa704d7a4885679833a2636cc707
Elard Guevara <elard.guevara@ucsp.edu.pe>
2015-11-03 01:09:49 -0500

    los demas ejercicios

---
+++ e11.adb
@@ -0,0 +1,10 @@
+with Ada.Text_IO; use Ada.Text_IO;
+procedure e11 is
+
+V: Boolean := TRUE;
+I, J: Integer := 1;
+Nombre: String := "Wikilibros";
+
+begin
+  Put_Line ("Hola, mundo!");
+end e11;
---
+++ e11.java
@@ -0,0 +1,14 @@
+class ejercicio11
+{
+	public static void main(String[] args) 
+	{
+		int a=10;
+		float b=3.5;
+
+		a=b;
+		System.out.println(a);
+
+	}
+}
+
+//gnatmake
