510b0398f697d40ce0c736e4be65546fc6b8d179
Elard Guevara <elard.guevara@ucsp.edu.pe>
2015-11-10 08:45:04 -0500

    avance

--- e5.cpp
+++ e5.cpp
@@ -4,13 +4,14 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	
-	enum Days { Saturday,Sunday,Tuesday,Wednesday,Thursday,Friday }; 
-	//enum Pos {Thursday, Friday, Sunday};//redeclarado
-
-	Days dia = Saturday;
-
-	dia = 453; //no permite
-
-	return 0;
+	enum dia { DOM, LUN, MART, MIERCOLES = 8, JUEV, VIER, SAB } diaX;
+    //enum { DOM, LUN, MART, MIERCOLES, JUEV, VIER, SAB } dia2X; error DOM, LUN, MART ... ya fueron declarados         
+    enum dia diapago;
+    int i = MIERCOLES;
+    diaX = LUN;
+    //diaX = 9; error, solo se puede asignar los dias
+    //LUN = MIER; error
+    //double DOM = 0; error ya declarada dom
+    int y = LUN + MART;
+    return 0;
 }
--- e5.cs
+++ e5.cs
@@ -2,11 +2,18 @@ using System;
 
 class e6
 {
-    enum Days { Saturday,Sunday,Tuesday,Wednesday,Thursday,Friday }; 
-	enum Pos {Thursday, Friday, Sunday};//redeclarado
+    enum dia { DOM, LUN, MART, MIERCOLES = 8, JUEV, VIER, SAB }; 
 
     static void Main()
     {
-		
+		dia diax;
+        //enum dia { DOM, LUN, MART, MIERCOLES, JUEV, VIER, SAB }; error
+        //enum { DOM, LUN, MART, MIERCOLES, JUEV, VIER, SAB } dia2X; error
+        int i = (int)dia.MIERCOLES;
+        diax = dia.LUN;
+        //diax = 9; error
+        //dia.LUN = dia.MIERCOLES; error
+        //dia.LUN = 5; error
+        int y = (int)dia.LUN + (int)dia.MART;
     }
 }
